87cd3d2f6d34bbf7f6d03c112e05a124f78f17a3
gencdevops <gencdevops@gmail.com>
2021-10-17 08:57:21 +0300

    Initial commit

---
+++ src/main/java/com/example/folksdevblog/FolksdevBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.folksdevblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolksdevBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FolksdevBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/folksdevblog/FolksdevBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.folksdevblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolksdevBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
