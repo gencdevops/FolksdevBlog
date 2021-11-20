fdd41fb9451ba704a52e4bd9e721547683ef8119
tanergenc <taner.genc@consultant.turkcell.com.tr>
2021-11-20 14:05:42 +0300

    Mapper classes added

---
+++ src/main/java/com/example/folksdevblog/dto/AuthorDTO.java
@@ -0,0 +1,14 @@
+package com.example.folksdevblog.dto;
+
+import com.example.folksdevblog.model.Blog;
+
+import java.util.Set;
+
+public class AuthorDTO {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Set<Blog> blogs;
+
+}
